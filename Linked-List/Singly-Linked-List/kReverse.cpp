5497c3ebf913faaf6429887b562a498bab2b209d
HarshilPatel-28 <122884548+HarshilPatel-28@users.noreply.github.com>
2023-01-31 21:58:19 +0530

    Create kReverse.cpp

---
+++ Linked-List/Singly-Linked-List/kReverse.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h> 
+/****************************************************************
+    Following is the Linked List node structure
+
+    class Node
+    {
+    public:
+        int data;
+        Node *next;
+        Node(int data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*****************************************************************/
+
+
+Node* kReverse(Node* head, int k) {
+    // Write your code here.
+    if(head == NULL){
+        return NULL;
+
+    }
+
+    Node* next= NULL;
+    Node* prev= NULL;
+    Node* curr= head;
+    int cnt=0;
+
+    while( curr != NULL && cnt<k){
+
+        next = curr -> next;
+        curr -> next = prev;
+        prev = curr;
+        curr = next;
+        cnt++;
+        
+    }
+
+    if(next !=NULL){
+        head -> next = kReverse(next, k);
+    }
+
+    return prev;
+
+}
